398e02f239a401e60b43ed17806cb45103afbca3
U-ANDRIJAN\Andrew <Andrew@andrijan.(none)>
2011-01-31 21:37:28 -0600

    Playing around with jDip in Scala.

--- src/jdip/rewritten/SymbolInjector.java
+++ src/jdip/rewritten/SymbolInjector.java
@@ -41,19 +41,8 @@ import java.util.*;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
-import org.apache.batik.util.XMLResourceDescriptor;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.w3c.dom.*;
-import org.w3c.dom.svg.SVGDocument;
 
 
 /**
