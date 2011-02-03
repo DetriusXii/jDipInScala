4461b15da9510e3f220d143ed9255db9936dc388
U-ANDRIJAN\Andrew <Andrew@andrijan.(none)>
2011-02-02 20:39:36 -0600

    Deleted the GameController/GameServer.scala from the project. Deleted the jDipConfiguration.properties file

--- src/jdip/scala/constants/Constants.scala
+++ src/jdip/scala/constants/Constants.scala
@@ -7,6 +7,17 @@ package jdip.scala.constants
 
 object Constants {
   val hostAddress : String = "localhost"
-  val hostPort : String ="5401"
+  val hostPort : Int = 5401
+  val savePath : String= "D:/cygwin/home/Andrew/jDipSaveFile/saves"
+  val variantPath : String = "D:/Program Files/jDip/variants"
+  val gameName : String = "FirstGame"
+  val nameToCountryTable : Map[String, String] = Map("Andrew" -> "Turkey",
+                                                     "Tim" -> "Germany",
+                                                     "Brandon" -> "Russia",
+                                                     "Danton" -> "France",
+                                                     "Ryan" -> "Austria",
+                                                     "Mark" -> "Italy",
+                                                     "Carey" -> "England");
+
 
 }
--- src/jdipinscala/Main.scala
+++ src/jdipinscala/Main.scala
@@ -10,87 +10,63 @@ import jdip.imageOutput.ImageCreator
 import java.util.Properties
 import java.io.File
 import java.io.FileInputStream
-import dip.gui.ClientFrame
 import GameData._
-import GameController._
-import java.net.ServerSocket
-import java.net.Socket
-import java.io.ObjectInputStream
-import scala.actors.remote.RemoteActor
+import jdip.scala.constants.Constants
 
+import scala.actors.Actor
+import scala.actors.remote.RemoteActor
 import scala.collection.JavaConversions._
 
-import dip.world.{Unit => _, _}
+import dip.world.{Unit => _, Map => _, _}
 import dip.gui.order2._
 import dip.order.Move
+import scala.util.parsing.json.JSONArray
+import scala.util.parsing.json.JSONObject
 
 object Main {
+
+  def getMovements(playerName : String) : JSONArray = {
+    val powerName = Constants.nameToCountryTable.get(playerName)
+    val fullGameName = String.format("%s/%s.jdip", Constants.savePath, Constants.gameName)
+    val f : File = new File(fullGameName)
+    val world : World = World.open(f)
+    val ts : TurnState = world.getLastTurnState
+    val position : Position = ts.getPosition
+    val worldMap : dip.world.Map = world.getMap
+    val playerPower : dip.world.Power = worldMap.getPowerMatching(powerName)
+
+    JSONArray(position.getUnitProvinces(playerPower).toList map ((p : Province) => {
+        val u : dip.world.Unit = position.getUnit(p)
+        JSONObject(Map("unitType" -> u.getType.toString, "provinceName" -> p.getFullName))
+      }))
+
+  }
 
   /**
    * @param args the command line arguments
    */
   def main(args: Array[String]) : Unit = {
-    val props : Properties = new Properties;
-    props.load(new FileInputStream(args(0) + "jDipConfiguration.properties"))
-    val savepath : String = props.getProperty("savepath")
-    val imagepath : String = props.getProperty("imagepath")
-    val gameName : String = props.getProperty("gameName")
-    val pName : String = props.getProperty("pName")
-    val variantPath : String = props.getProperty("variantPath")
-    val fullGameName : String = String.format("%s%s", savepath, gameName)
-    val f : File = new File(fullGameName);
+    val hostName : String = Constants.hostAddress
 
-    val world : World = World.open(f);
-    val ts : TurnState = world.getLastTurnState
-    val position : Position = ts.getPosition
-    val worldMap : dip.world.Map = world.getMap
-    val francePower : Power = worldMap.getPowerMatching(pName);
-
-    val gOrderFactory : GUIOrderFactory = new GUIOrderFactory
-
-    val srcProvince : Province = worldMap.getProvinceMatching("mar")
-    val srcUnit : dip.world.Unit = position.getUnit(srcProvince)
-    val srcLocation : dip.world.Location = 
-      new Location(srcProvince, srcUnit.getCoast)
-    val destProvince : Province = worldMap.getProvinceMatching("bur")
-    val destLocation : dip.world.Location =
-      new Location(destProvince, Coast.NONE)
-
-    /*setting some moves for France and resolving*/
-    val mv1 : Move =
-      gOrderFactory.createMove(francePower,
-                               srcLocation,
-                               srcUnit.getType,
-                               destLocation)
-
-    val srcProvince2 : Province = worldMap.getProvinceMatching("par")
-    val srcUnit2 : dip.world.Unit = position.getUnit(srcProvince2)
-    val srcLocation2 : dip.world.Location =
-      new Location(srcProvince2, srcUnit2.getCoast)
-    val destProvince2 : Province = worldMap.getProvinceMatching("pic")
-    val destLocation2 : dip.world.Location =
-      new Location(destProvince2, Coast.NONE)
-
-    val mv2 : Move = gOrderFactory.createMove(francePower, srcLocation2, srcUnit2.getType, destLocation2)
-
-    val srcProvince3 : Province = worldMap.getProvinceMatching("bre")
-    val srcUnit3 : dip.world.Unit = position.getUnit(srcProvince3)
-    val srcLocation3 : dip.world.Location = new Location(srcProvince3, srcUnit3.getCoast)
-    val destProvince3 : Province = worldMap.getProvinceMatching("eng")
-    val destLocation3 : dip.world.Location = new Location(destProvince3, Coast.SEA)
-    val mv3 : Move = gOrderFactory.createMove(francePower, srcLocation3, srcUnit3.getType, destLocation3)
-
-    val orderList : List[Move] = mv1 :: mv2 :: mv3 :: Nil
-
-    val orderListJava : java.util.List[Move] = orderList
-
-    ts.setOrders(francePower, orderListJava)
-
-    val imgCreator : ImageCreator = new ImageCreator(savepath, variantPath, false)
+    println("Starting server")
     
-    imgCreator.process("trialWorld", world)
+    val gameServerActor = Actor.actor {
+      RemoteActor.alive(Constants.hostPort)
+      RemoteActor.register(Symbol("myname"), Actor.self)
 
-    println(ts)
+      def waitForMessage : Unit = Actor.react {
+        case Some((x : String, "getMovements")) =>
+          val movements = getMovements(x)
+          Actor.sender ! movements
+          waitForMessage
+        case _ =>
+          println("nonstandard game message")
+          waitForMessage
+      }
+
+      println("Waiting for message")
+      waitForMessage
+    }
   }
 
 }
